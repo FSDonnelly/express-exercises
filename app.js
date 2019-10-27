cfe460ba83dda7de5dee1637c56fc1fb19cfff01
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-26 19:40:21 -0700

    function defs and examples in script.js reformated app.js with arrow functions

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => {
-  res.send(`<h1>Home Page</h1>`);
-});
+app.get('/', (req, res) => res.send(`<h1>Home Page</h1>`));
 
-app.get('/contacts', (req, res) => {
-  res.send(`<h1>Contacts Page</h1>`);
-});
+app.get('/contacts', (req, res) => res.send(`<h1>Contacts Page</h1>`));
 
-app.get('*', (req, res) => {
-  res.send(`<h1>404 Page Not Found</h1>`); //Written below all other get methods
-});
+app.get('*', (req, res) => res.send(`<h1>404 Page Not Found</h1>`)); //Written below all other get methods
 
 const PORT = process.env.PORT || 3000;
 
