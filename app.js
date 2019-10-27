71bd5a9a040af46aa1e5fded8c114ee3bd9db891
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-26 18:57:16 -0700

    initial setup express in app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+app.get('/', (req, res) => {
+  res.send(`<h1>Home Page</h1>`);
+});
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(3000, () => {
-  console.log('Listening on port 3000...');
+  console.log(`Listening on http://localhost:${PORT}/`);
 });
