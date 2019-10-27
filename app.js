4d1721d6c982a7b0f68f370024bb8d3a9e429f53
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-26 19:04:43 -0700

    add contacts route and 404 route in app.js

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get('/', (req, res) => {
   res.send(`<h1>Home Page</h1>`);
 });
 
+app.get('/contacts', (req, res) => {
+  res.send(`<h1>Contacts Page</h1>`);
+});
+
+app.get('*', (req, res) => {
+  res.send(`<h1>404 Page Not Found</h1>`); //Written below all other get methods
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
