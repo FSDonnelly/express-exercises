11be9a297de71dabcc30c1a3ac3fd4397b9fcac7
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-27 17:29:56 -0700

    route parameter express exercerises

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => res.send(`<h1>Home Page</h1>`));
+// app.get('/', (req, res) => res.send(`<h1>Home Page</h1>`));
 
-app.get('/contacts', (req, res) => res.send(`<h1>Contacts Page</h1>`));
+// app.get('/contacts', (req, res) => res.send(`<h1>Contacts Page</h1>`));
 
-app.get('*', (req, res) => res.send(`<h1>404 Page Not Found</h1>`)); //Written below all other get methods
+// app.get('*', (req, res) => res.send(`<h1>404 Page Not Found</h1>`)); //Written below all other get methods
+
+// Examples of Route Parameters
+
+app.get('/shop/tv', (req, res) => {
+  res.send('<h4>Category: TV</h4>');
+});
+
+app.get('/shop/laptop', (req, res) => {
+  res.send('<h4>Category: Laptop</h4>');
+});
+
+app.get('/shop/phone', (req, res) => {
+  res.send('<h4>Category: Phone</h4>');
+});
 
 const PORT = process.env.PORT || 3000;
 
