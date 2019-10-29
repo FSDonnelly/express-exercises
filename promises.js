2599ab1ab60747b779efd5cba18bf09a1952201b
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-28 21:26:12 -0700

    promises.js using resolve, reject, .then, and .catch exercises

--- promises.js
+++ promises.js
@@ -13,3 +13,23 @@ printNumber = num => {
 };
 
 getRandomNumber(1, 10).then(printNumber);
+
+getOddRandomNumber = (min, max) => {
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let random = Math.floor(Math.random() * (max - min + 1) + min);
+      if (random % 2 === 1) {
+        resolve(random);
+      } else {
+        reject(`The number ${random} is even`);
+      }
+    }, 2000);
+  });
+  return promise;
+};
+
+getOddRandomNumber(1, 10)
+  .then(printNumber)
+  .catch(msg => {
+    console.log(msg);
+  });
