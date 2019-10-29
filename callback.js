b8a77ef8b4a290da91213a75d6a0f643478c5212
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-28 21:06:13 -0700

    callback.js using next exercises

--- callback.js
+++ callback.js
@@ -1,11 +1,15 @@
-getRandomNumber = (min, max) => {
-  let random = Math.floor(Math.random() * (max - min + 1) + min);
-  return random;
+getRandomNumber = (min, max, next) => {
+  setTimeout(() => {
+    let random = Math.floor(Math.random() * (max - min + 1) + min);
+    // return random;
+    next(random);
+  }, 2000);
 };
 
 printNumber = num => {
   console.log(num);
 };
 
-let randomNumber = getRandomNumber(1, 10);
-printNumber(randomNumber);
+// let randomNumber = getRandomNumber(1, 10);
+// printNumber(randomNumber);
+getRandomNumber(1, 10, printNumber);
