f95dc0a45b808bfbaa638d55d638bb3f62715176
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-28 20:32:03 -0700

    import and export module exercises array example

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 let shop = require(`./shop`);
 
 shop.calCost(5, 3);
+console.log(shop.myGoods[0]);
--- shop.js
+++ shop.js
@@ -1,7 +1,10 @@
+let goods = [`apple`, `orange`, `avacado`];
+
 function calCost(price, number) {
   let cost = price * number;
   console.log(`Cost: $${cost}`);
 }
 
 module.exports.calCost = calCost;
+module.exports.myGoods = goods;
 // console.log(module);
