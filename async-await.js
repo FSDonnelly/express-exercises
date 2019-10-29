6b2d3a0122e3730e2427af4197ef9b028252f30e
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-28 21:40:35 -0700

    async-await.js using try catch exercises

--- async-await.js
+++ async-await.js
@@ -17,3 +17,27 @@ func = async () => {
   printNumber(randomNumber);
 };
 func();
+
+getOddRandomNumber = (min, max) => {
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let random = Math.floor(Math.random() * (max - min + 1) + min);
+      if (random % 2 === 1) {
+        resolve(random);
+      } else {
+        reject(`The number ${random} is even`);
+      }
+    }, 2000);
+  });
+  return promise;
+};
+
+func2 = async () => {
+  try {
+    let randomNumber = await getOddRandomNumber(1, 10);
+    printNumber(randomNumber);
+  } catch (msg) {
+    console.log(msg);
+  }
+};
+func2();
