686e6fac1527ff09bc47139c83ee9967fc86caa9
FSDonnelly <donnelly_frank@yahoo.com>
2019-10-31 22:24:57 -0700

    CRUD exercises. Task 1 complete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 mongoose
-  .connect(`mongodb://localhost/users`, {
+  .connect(`mongodb://localhost/wikipedia`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
@@ -14,21 +14,95 @@ mongoose
     console.log(`Something went wrong!!!!@#$%`);
   });
 
-let userSchema = new Schema({
-  login: String,
-  password: {
-    type: String,
-    minlength: 4
-  },
-  age: {
-    type: Number,
-    min: 0
-  },
-  student: Boolean,
-  country: String
+let countrySchema = new Schema({
+  country: String,
+  capital: String,
+  area: Number,
+  population: Number,
+  currency: String
 });
 
-let User = mongoose.model('User', userSchema);
+let Country = mongoose.model('Country', countrySchema);
+
+let countries = [
+  {
+    country: 'USA',
+    capital: 'Washington',
+    area: 9833520,
+    population: 327167434,
+    currency: 'USD'
+  },
+  {
+    country: 'Italy',
+    capital: 'Rome',
+    area: 301340,
+    population: 60483973,
+    currency: 'EUR'
+  },
+  {
+    country: 'Germany',
+    capital: 'Berlin',
+    area: 357386,
+    population: 83000000,
+    currency: 'EUR'
+  },
+  {
+    country: 'Canada',
+    capital: 'Ottawa',
+    area: 3855100,
+    population: 37242571,
+    currency: 'CAD'
+  },
+  {
+    country: 'China',
+    capital: 'Beijing',
+    area: 9596961,
+    population: 1403500365,
+    currency: 'CNY'
+  },
+  {
+    country: 'Sweden',
+    capital: 'Stockholm',
+    area: 450295,
+    population: 10223505,
+    currency: 'SEK'
+  },
+  {
+    country: 'India',
+    capital: 'New Delhi',
+    area: 3287263,
+    population: 1324171354,
+    currency: 'INR'
+  },
+  {
+    country: 'Netherlands',
+    capital: 'Amsterdam',
+    area: 41543,
+    population: 17302139,
+    currency: 'EUR'
+  }
+];
+
+countries.forEach(value => {
+  let country = new Country(value);
+  country.save();
+});
+
+// let userSchema = new Schema({
+//   login: String,
+//   password: {
+//     type: String,
+//     minlength: 4
+//   },
+//   age: {
+//     type: Number,
+//     min: 0
+//   },
+//   student: Boolean,
+//   country: String
+// });
+
+// let User = mongoose.model('User', userSchema);
 
 getUsers = async () => {
   let users = await User.find()
@@ -43,7 +117,7 @@ getUsers = async () => {
 
 // To find alll mongoose functions goto mongoose.com -> API -> Query
 
-getUsers();
+// getUsers();
 
 updateUser = async () => {
   // Option 1
@@ -65,4 +139,4 @@ removeUser = async () => {
   console.log(result);
 };
 
-removeUser();
+// removeUser();
